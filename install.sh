d054b83a1ce11dae763fc4aaae04a8e498085bfb
Lee JongYoung <leejongyoung@icloud.com>
2018-12-15 00:11:12 +0900

    Fix install script

--- install.sh
+++ install.sh
@@ -1,8 +1,57 @@
 #!/usr/bin/env bash
 
+ESC_SEQ="\x1b["
+COL_RESET=$ESC_SEQ"39;49;00m"
+COL_RED=$ESC_SEQ"31;01m"
+COL_GREEN=$ESC_SEQ"32;01m"
+COL_YELLOW=$ESC_SEQ"33;01m"
+COL_BLUE=$ESC_SEQ"34;01m"
+COL_MAGENTA=$ESC_SEQ"35;01m"
+COL_CYAN=$ESC_SEQ"36;01m"
+
+function ok() {
+  echo -e "$COL_GREEN[ok]$COL_RESET "$1
+}
+
+function bot() {
+  echo -e "\n$COL_GREEN\[._.]/$COL_RESET - "$1
+}
+
+function running() {
+  echo -en "$COL_YELLOW ⇒ $COL_RESET"$1": "
+}
+
+function action() {
+  echo -e "\n$COL_YELLOW[action]:$COL_RESET\n ⇒ $1..."
+}
+
+function warn() {
+  echo -e "$COL_YELLOW[warning]$COL_RESET "$1
+}
+
+function error() {
+  echo -e "$COL_RED[error]$COL_RESET "$1
+}
+
+bot "checking homebrew install"
+brew_bin=$(which brew) 2>&1 > /dev/null
+if [[ $? != 0 ]]; then
+  action "installing homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    if [[ $? != 0 ]]; then
+      error "unable to install homebrew, script $0 abort!"
+      exit 2
+  fi
+else
+  echo "homebrew installed"
+  ok
+  bot "updating homebrew"
+  brew update
+  ok
+fi
 IFS=$'\n' fonts=(`curl -Ls https://raw.githubusercontent.com/leejongyoung/homebrew-korea-fonts/master/install_font_list.txt`)
 for font in "${fonts[@]}"; do
-  echo "- Install $font"
+  action "Install $font"
   brew cask install $font
 done
-echo "Done !!"
+bot "font install complete !!"
