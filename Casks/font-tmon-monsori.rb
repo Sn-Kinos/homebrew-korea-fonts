4360181d89eb0012be359fc1c4bb1784a44d125c
kissd3v <kissd3v@gmail.com>
2018-12-16 02:43:14 +0900

    Release resolve #12

--- Casks/font-tmon-monsori.rb
+++ Casks/font-tmon-monsori.rb
@@ -1,8 +1,10 @@
 cask 'font-tmon-monsori' do
-    version : latest
-    sha : no_check
-    url : "http://t1.daumcdn.net/brunch/service/user/12G8/file/Cfo4X30rXaUSHA_jNr25MV_6sS4.zip?download"
-    name : 'monsori'
+    version :latest
+    sha :no_check
+    url "http://t1.daumcdn.net/brunch/service/user/12G8/file/Cfo4X30rXaUSHA_jNr25MV_6sS4.zip?download"
+    name 'monsori'
     homepage 'https://brunch.co.kr/@creative/32'
-    font "TmonMonsori.otf"
+    font "TmonMonsori.otf.otf"
+    font "TmonMonsori.ttf.ttf"
+    font "TmonTium.ttf"
 end
