f81d54113db112edb3390c50ff08b9b641047959
kissd3v <kissd3v@gmail.com>
2018-12-15 20:35:42 +0900

    Release resolove #12

--- Casks/font-tmon-monsori.rb
+++ Casks/font-tmon-monsori.rb
@@ -6,18 +6,3 @@ cask 'font-tmon-monsori' do
  homepage 'https://brunch.co.kr/@creative/32'
  font "TmonMonsori.otf"
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-^G Get Help  ^O WriteOut  ^R Read File ^Y Prev Page ^K Cut Text  ^C Cur Pos
-^X Exit      ^J Justify   ^W Where Is  ^V Next Page ^U UnCut Text^T To Spell
