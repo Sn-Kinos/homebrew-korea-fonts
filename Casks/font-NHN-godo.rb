309099287a02f63dc9d33eff8aeb15bf5ee55bd3
kissd3v <kissd3v@gmail.com>
2018-12-17 22:29:58 +0900

    Release resolve #23

--- Casks/font-NHN-godo.rb
+++ Casks/font-NHN-godo.rb
@@ -1,6 +1,6 @@
-cask 'font-godo' do
+cask 'font-nhn-godo' do
     version :latest
-    sha :no_check
+    sha256 :no_check
     url "http://static.godo.co.kr/download/designCenter/free-font/godoFont_all.zip"
     name 'godo'
     homepage 'https://design.godo.co.kr/custom/free-font.php'
