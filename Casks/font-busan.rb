a7a235e42a08d0c4708d1d55823c0b5e3c5740df
LeeNamYong <kissd3v@gmail.com>
2018-12-20 16:31:57 +0900

    Delete font-busan.rb

--- Casks/font-busan.rb
+++
@@ -1,8 +0,0 @@
-cask 'font-busan' do
-    version :latest
-    sha256 :no_check
-    url "https://www.busan.go.kr/PageDownload.do?savename=bFont_mProvisional_otf.zip"
-    name 'busan'
-    homepage 'https://www.busan.go.kr/index'
-    font "BusanFont_Provisional.ttf"
-end
