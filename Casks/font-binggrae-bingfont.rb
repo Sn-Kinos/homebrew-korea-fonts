6372c62a913577b98c8a26807d90f9e920703798
Kinos <dony435@naver.com>
2020-07-20 01:11:46 +0900

    Update font-binggrae-bingfont.rb

--- Casks/font-binggrae-bingfont.rb
+++ Casks/font-binggrae-bingfont.rb
@@ -1,8 +1,8 @@
-cask 'font-binggra-bingfont' do
+cask 'font-binggrae-bingfont' do
     version :latest
     sha256 :no_check
     url "http://kiss.i234.me:60606/fsdownload/OnfTjAmHd/Binggrae.zip"
     name 'Seoul Hangang, Namsan'
     homepage 'http://www.bingfont.co.kr/bingfont.html'
-    pkg "서울서체 2018  전용서체 통합본.mpkg"
+    pkg "서울서체 2018  전용서체 통합본.mpkg"
 end
